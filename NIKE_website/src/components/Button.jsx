c663b1ab32c4c09d5780780ad7367c10d480c3aa
GarimaUttam <garimauttam191203@gmail.com>
2023-09-28 19:14:32 +0530

    added subscribe section

--- NIKE_website/src/components/Button.jsx
+++ NIKE_website/src/components/Button.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Button = ({label,iconURL,backgroundColor, textColor,borderColor}) => {
+const Button = ({label,iconURL,backgroundColor, textColor,borderColor,fullWidth}) => {
   return (
     <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
     ${
       backgroundColor
-      ? `${backgroundColor} ${textColor} ${borderColor}`:" bg-coral-blue text-white border-coral-blue"} rounded-full w-full"}`}>{label}
+      ? `${backgroundColor} ${textColor} ${borderColor}`:" bg-coral-blue text-white border-coral-blue"} rounded-full ${ fullWidth && `w-full`}"}`}>{label}
     </button>
   )
 }
--- NIKE_website/src/sections/Subscribe.jsx
+++ NIKE_website/src/sections/Subscribe.jsx
@@ -1,6 +1,19 @@
+import Button from "../components/Button"
 const Subscribe = () => {
   return (
-    <div>Subscribe</div>
+    <section className="max-container flex justify-between items-center max-lg:flex-col px-5 gap-10 py-[3px] mt-[1px]" id="contact-us">
+      <h3 className="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold">Sign Up from <span className="text-coral-blue"> 
+          Updates
+        </span> & Newsletter
+      </h3>
+      <div className="lg:max-w-[50%] w-full flex items-center max-sm:flex-col gap-5 p-2 sm:border sm:border-slate-gray rounded-full ">
+        <input type="text"
+        placeholder="subscribe$runon.com" className="input" />
+        <div className="flex max-sm:justify-end items-center max-sm:w-full">
+          <Button label="Sign Up" fullWidth />
+        </div>
+      </div>
+    </section> 
   )
 }
 
