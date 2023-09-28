d97f627a45f80db54ed3c5e4d83f6b58c51d38a9
GarimaUttam <garimauttam191203@gmail.com>
2023-09-28 14:49:28 +0530

    added Services page

--- NIKE_website/src/components/PopularProductCard.jsx
+++ NIKE_website/src/components/PopularProductCard.jsx
@@ -3,7 +3,7 @@ import { star } from "../assets/icons";
 
 const PopularProductCard = ({imgURL,name ,price}) => {
   return (
-    <div className="flex flex-1 flex-col w-full max-sm:w-full">
+    <div className="flex flex-1 flex-col w-full max-sm:w-full mx-9">
       <img
         src={imgURL} alt={name}
         className='w-[280px] h-[280px]'
--- NIKE_website/src/components/index.js
+++ NIKE_website/src/components/index.js
@@ -2,8 +2,12 @@ import Buttons from "./Buttons";
 import Nav from "./Nav";
 import ShoeCard from "./ShoeCard";
 import PopularProductCard from "./PopularProductCard";
+import Button from "./Button";
+import ServiceCard from "./ServiceCard";
 export{
     Buttons,
     Nav,
-    ShoeCard,PopularProductCard
+    ShoeCard,PopularProductCard,
+    Button,
+    ServiceCard
 }
