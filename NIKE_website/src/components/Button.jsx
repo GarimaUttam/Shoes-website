1a0aced35d3d166a5e3386b93e5d69d8080fcbdb
GarimaUttam <garimauttam191203@gmail.com>
2023-09-28 16:21:25 +0530

    modified superquality

--- NIKE_website/src/components/Button.jsx
+++ NIKE_website/src/components/Button.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-const Button = ({label,iconURL}) => {
+const Button = ({label,iconURL,backgroundColor, textColor,borderColor}) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-blue rounded-full text-white border-coral-blue">{label}
+    <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+    ${
+      backgroundColor
+      ? `${backgroundColor} ${textColor} ${borderColor}`:" bg-coral-blue text-white border-coral-blue"} rounded-full w-full"}`}>{label}
     </button>
   )
 }
